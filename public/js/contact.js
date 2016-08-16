628354bb448c6427b5a7a1847412670436d6fd0d
Bryant Oblad <bryant.oblad@gmail.com>
2016-08-16 09:01:36 -0600

    adding in some content

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,5 +1,25 @@
 ;(function ($){
     $(function() {
+      // $('#subject').focus(function() {
+      //   $('#subject').addClass('contact__focussed');
+      // });
+      // $('#subject').focusout(function() {
+      //   $('#subject').removeClass('contact__focussed');
+      // });
+      //
+      // $('#message').focus(function() {
+      //   $('#message').addClass('contact__focussed');
+      // });
+      // $('#message').focusout(function() {
+      //   $('#message').removeClass('contact__focussed');
+      // });
+      //
+      // $('#email').focus(function() {
+      //   $('#email').addClass('contact__focussed');
+      // });
+      // $('#email').focusout(function() {
+      //   $('#email').removeClass('contact__focussed');
+      // });
 
     });
 })(jQuery);
